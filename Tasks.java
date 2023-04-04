5da7f6cb24738907bbe8934bc5d53401d29c2354
Alex <best1972@list.ru>
2023-04-04 16:04:23 +0300

    made tasks 2

--- Tasks.java
+++ Tasks.java
@@ -2,7 +2,8 @@
 // import java.util.Iterator;
 // import java.util.Random;
 
-// Задача 1.Пусть дан произвольный список целых чисел, удалить из него чётные числа.   
+// Задача 1. Пусть дан произвольный список целых чисел, удалить из него чётные числа.
+
 // public class Tasks {
 //     public static void main(String[] args) {
 //         ArrayList<Integer> numbers = new ArrayList<>();
@@ -20,4 +21,23 @@
 //         System.out.printf("Список с удаленными чётными числами %s", numbers);
 //     }
 // }
-    
+
+// Задача 2. Задан целочисленный список ArrayList. Найти минимальное, максимальное и 
+// среднее ариф. из этого списка. 
+
+// public class Tasks {
+//     public static void main(String[] args) {
+//         ArrayList<Integer> numbers = new ArrayList<>();
+//         Random random = new Random();
+//         for (int i = 0; i < 15; i++) {
+//             numbers.add(random.nextInt(20));
+//         }
+//         System.out.printf("Целочисленный список %s" + "\n", numbers);
+//         numbers.sort(null);
+//         System.out.printf("Минимальное число: %s" + "\n", numbers.get(0));
+//         System.out.printf("Максимальное число: %s" + "\n", numbers.get(numbers.size() - 1));
+//         double sum = numbers.stream().mapToInt(Integer::intValue).sum();
+//         double arg = sum / numbers.size();
+//         System.out.printf("Среднее арифметическое: %s", arg);
+//     }
+// }
